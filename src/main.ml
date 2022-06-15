2eff7676f3d412e9e010b4a1ccea60d83009967b
Michael C Vanier <mvanier@cms.caltech.edu>
2022-06-14 18:29:26 -0700

    Updates.

--- src/main.ml
+++ src/main.ml
@@ -1,52 +1,27 @@
 open Clclib
 
 open Types
-open Lexer_utils
-open Parser_utils
 open Eval
 
 (* The REPL, in the form expected by Repl.make_repl. *)
-let eval_fn def =
+let eval_fn form =
   try
-    eval_def def
+    match eval_form form with
+      | None -> ()
+      | Some e -> Printf.printf "%s\n%!" (string_of_expr2 e)
   with Failure msg -> Printf.eprintf "%s\n%!" msg
 
 let progname = "clc"
 
-let handle_fatal_parse_errors result =
-  match result with
-    | Incomplete ->
-      begin
-        Printf.eprintf
-          "Incomplete form at end of input; exiting.\n%!";
-        exit 1
-      end
-    | Error (l, msg) ->
-      begin
-        print_error l msg;
-        exit 1
-      end
-    | Ok x -> x
-
-let handle_fatal_exceptions thunk =
-  try
-    thunk ()
-  with
-    Lexer_error (l, err) ->
-      begin
-        print_error l (string_of_lex_error err);
-        exit 1
-      end
-
 let prompt1 = "   "
 let prompt2 = "...  "
 
-let make_repl vm eval_fn =
+let make_repl eval_fn =
   let error_fn () = () in
     begin
       try
         ignore
-          (Repl.make_repl prompt1 prompt2 eval_fn error_fn vm)
+          (Repl.make_repl prompt1 prompt2 eval_fn error_fn)
       with
         End_of_file ->
           begin
@@ -57,14 +32,17 @@ let make_repl vm eval_fn =
     end
 
 (* Add some derived definitions. *)
-let _ = add_to_env "M" (List [w; i])
-let _ = add_to_env "A" (List [k; i])
-let _ = add_to_env "T" (List [c; i])
+
+let prim p = Atom (Prim p)
+
+let _ = add_to_env "M" (List [prim W; prim I])
+let _ = add_to_env "A" (List [prim K; prim I])
+let _ = add_to_env "T" (List [prim C; prim I])
 
 (* Entry point. *)
 let _ =
   match Sys.argv with
-    | [| _ |] -> make_repl vm eval_fn
+    | [| _ |] -> make_repl eval_fn
     | _ ->
       begin
         Printf.eprintf "usage: %s\n%!" progname;
