62406ead7a01c0e96ad74cbba38b25da50ef0d43
Michael C Vanier <mvanier@cms.caltech.edu>
2024-08-05 22:56:11 -0700

    Updates.

--- src/clc2/ir.mli
+++ src/clc2/ir.mli
@@ -1,4 +1,22 @@
-(* Intermediate representation 1. *)
+(** Intermediate representation 1. *)
+
+(*
+
+This IR is responsible for expanding out function applications
+into a more explicit form.  Specifically, it makes the implicit
+left-folding of applications explicit. So:
+
+```
+(a b c d e)
+```
+
+becomes:
+
+```
+((((a b) c) d) e)
+```
+
+*)
 
 type id = string
 
