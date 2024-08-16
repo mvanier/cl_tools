da4e4d208f73460b0f3678ee0da8e09cfd295a1d
Michael C Vanier <mvanier@cms.caltech.edu>
2024-08-16 16:22:54 -0700

    Added the Lark (L) combinator to the basis.

--- src/clc2/basis.ml
+++ src/clc2/basis.ml
@@ -1,12 +1,13 @@
 let basis = "
-def I x = x;
-def K x y = x;
 def B f g x = f (g x);
 def C f x y = f y x;
-def W f x = f x x;
+def I x = x;
+def K x y = x;
+def L f x = f (x x);
+def M x = x x;
+def Q = K I;
 def S f g x = f x (g x);
+def W f x = f x x;
 def X x = x K S K;
 def Y x = x S K;
-def Q = K I;
-def M x = x x;
 "
