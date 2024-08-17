a57602c1ad1c899ba1bf3867b45cac0fac41d186
Michael C Vanier <mvanier@cms.caltech.edu>
2024-08-17 16:04:56 -0700

    Updates.

--- src/coq/TMAM_9.v
+++ src/coq/TMAM_9.v
@@ -1,2 +1,33 @@
 (* Coq proofs of problems in _To Mock a Mockingbird_, chapter 9. *)
 
+(*** Problem 1 ***)
+
+(* Informal proof: *)
+
+(*
+
+Given:
+
+  - the M combinator
+  - the composition condition C1
+
+then for an arbitrary combinator [x],
+find [F], the fixpoint of [x], such that [x F = F].
+
+The composition condition is:
+
+  forall A B, exists C, forall x, C x = A (B x).
+
+Solution:
+
+  Given x and M, compose x and M to get A:
+
+    Define [A = compose(x, M)]
+    --> A y = x (M y)
+    Define [F = M A]
+    --> F = M A = A A = x (M A) = x F
+    --> F = x F
+    Therefore [F] is the fixpoint of [x]. QED.
+
+*)
+
